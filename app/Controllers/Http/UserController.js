ad0dbc8a6738596b8f44c5d7056ce519bbd8578b
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-03-14 01:24:24 -0300

    Style: Added eslint

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const User = use('App/Models/User');
+const User = use('App/Models/User')
 
 class UserController {
-  async store({ request }) {
+  async store ({ request }) {
     const data = request.only(['username', 'email', 'password'])
 
     const user = await User.create(data)
